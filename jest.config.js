31c97a8b6516530655fe8b68ff74babe1981b77e
hayato shimizu <happylife5385@gmail.com>
2023-07-26 20:37:36 +0900

    fix

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  moduleFileExtensions: ["js", "ts", "json", "vue"],
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
-};
